959652c222e570d1dd722bc00355ed483fffe54c
richard-douglas <richard.soares@itec.ufpa.br>
2024-01-10 11:38:53 -0300

    Adiciona validação dos dados

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Support; 
+use App\Http\Requests\StoreUpdateSupport;
 use Illuminate\Http\Request;
 
 class SupportController extends Controller
@@ -32,9 +33,9 @@ class SupportController extends Controller
         return view('admin.support.create');
     }
 
-    public function store(Request $request, Support $support)
+    public function store(StoreUpdateSupport $request, Support $support)
     {
-        $data = $request->all();
+        $data = $request->validated();
         $data['status'] = 'a';
 
         $support = $support->create($data);
@@ -51,16 +52,13 @@ class SupportController extends Controller
         return view('admin.support.edit', compact('support'));
     }
 
-    public function update(Request $request, Support $support, string|int $id)
+    public function update(StoreUpdateSupport $request, Support $support, string|int $id)
     {
         if(!$support = $support::where('id', $id)->first()) {
             return back();
         }
 
-        $support->update($request->only([
-            'subject',
-            'body'
-        ]));
+        $support->update($request->validated());
 
         return redirect()->route('supports.index');
     }
